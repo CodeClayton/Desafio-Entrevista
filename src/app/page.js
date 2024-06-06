9dcb249b1702c5f9ab0921323fed07599b4dbcb5
CodeClayton <dev.clayton018@gmail.com>
2024-06-05 23:39:58 -0300

    Criacao componente Header

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Header from '../components/Header/Header'
 
 export default function Home() {
   return (
-    <h1>Ola mundo</h1>
+    <>
+      <Header />
+    </>
   );
 }
