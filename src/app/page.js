c355eb7d4243cbdd00aa5aa8fe6deb9008972468
CodeClayton <dev.clayton018@gmail.com>
2024-06-06 22:33:03 -0300

    Main em construção

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../components/Header/Header'
 import MainContent from "../components/mainContent/MainContent";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <MainContent /> 
+      <Footer />
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,30 @@
-import './header.css'
-import Button from '../Button/Button'
+'use client'
 
-export default function Header(){
+import './header.css'
+import React, { useState, useEffect } from 'react';
+import Button from '../Button/Button';
+
+export default function Header() {
+
+    const [theme, setTheme] = useState('light');
+
+    function toggleTheme() {
+        setTheme(prevTheme => {
+            const newTheme = prevTheme === 'light' ? 'dark' : 'light';
+            return newTheme;
+        });
+    }
+
+    useEffect(() => {
+        console.log(theme);
+        if (theme === 'dark') {
+            document.body.classList.add('darkTheme');
+        } else {
+            document.body.classList.remove('darkTheme');
+        }
+        
+        localStorage.setItem('theme', theme);
+    }, [theme]);
 
     return(
         <>
@@ -12,7 +35,14 @@ export default function Header(){
                 <a className="navLink" href="#">Disciplinas isoladas</a>
                 <a className="navLink" href="#">Cursos Grátis</a>
                 <a className="navLink" href="#">Bolsas de estudo</a>
-                <i className="bi bi-circle-half"></i>
+                <i className="bi bi-circle-half" id='togleTheme' onClick={toggleTheme}></i>
+            </nav>
+            <nav className="navMobile">
+                <i className="bi bi-chevron-down"></i>
+                <a href='/'>
+                    <img src='/img/logo.png' className='logoMob' ></img>
+                </a>
+                <i  className={`bi bi-circle-half ${theme === 'dark' ? 'darkTheme' : ''}`} id='togleThemeMob' ></i>
             </nav>
             <header>
                 <a href='/'>
@@ -33,6 +63,18 @@ export default function Header(){
 
                 <Button name="Já sou Aluno"/>
             </header>
+
+            <div className="headerMob">
+                <div className="hamburguerMenu">
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+                <div className="searchBox Mob">
+                    <i className="bi bi-search"></i>
+                    <input className='inputBox Mob' type="text" placeholder='Pesquise por um curso'/>
+                </div>
+            </div>
         </>
     )
 
