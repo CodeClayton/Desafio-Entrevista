a016458504b84eedb521c849ed83f50561590ac2
CodeClayton <dev.clayton018@gmail.com>
2024-06-08 00:38:03 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Header from '../components/Header/Header'
-import MainContent from "../components/mainContent/MainContent";
+import MainContent from "../components/MainContent/MainContent";
 import Footer from "..//components/Footer/Footer";
 
 export default function Home() {
