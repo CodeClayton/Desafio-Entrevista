0f05fcd20dc1a4b44e32469c7bc0eead5acf0a27
CodeClayton <dev.clayton018@gmail.com>
2024-06-08 00:31:25 -0300

    Criação dos sliders e responsividade aplicada

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,14 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../components/Header/Header'
 import MainContent from "../components/mainContent/MainContent";
-import Footer from "@/components/Footer/Footer";
+import Footer from "..//components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <MainContent /> 
-      <Footer />
+      <Footer /> 
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,53 @@
 'use client'
 
-import './header.css'
 import React, { useState, useEffect } from 'react';
+import './header.css';
 import Button from '../Button/Button';
 
 export default function Header() {
-
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState('');
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isHeaderOpen, setIsHeaderOpen] = useState(false);
 
     function toggleTheme() {
-        setTheme(prevTheme => {
-            const newTheme = prevTheme === 'light' ? 'dark' : 'light';
-            return newTheme;
-        });
+        setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+    }
+
+    function toggleMenu() {
+        setIsHeaderOpen(!isHeaderOpen);
+        if (isHeaderOpen) {
+            setIsHeaderOpen(false);
+        } else if (isMenuOpen) {
+            setIsMenuOpen(false);
+        }
+    }
+
+    function toggleNavTop() {
+        setIsMenuOpen(!isMenuOpen);
+        if (isHeaderOpen) {
+            setIsHeaderOpen(false);
+        } else if (isMenuOpen) {
+            setIsMenuOpen(false);
+        }
     }
 
     useEffect(() => {
-        console.log(theme);
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme) {
+            setTheme(savedTheme);
+        }
+    }, []);
+
+    useEffect(() => {
         if (theme === 'dark') {
             document.body.classList.add('darkTheme');
         } else {
             document.body.classList.remove('darkTheme');
         }
-        
         localStorage.setItem('theme', theme);
     }, [theme]);
 
-    return(
+    return (
         <>
             <nav className="navTopBar">
                 <a className="navLink" href="#">Graduação</a>
@@ -35,15 +56,23 @@ export default function Header() {
                 <a className="navLink" href="#">Disciplinas isoladas</a>
                 <a className="navLink" href="#">Cursos Grátis</a>
                 <a className="navLink" href="#">Bolsas de estudo</a>
-                <i className="bi bi-circle-half" id='togleTheme' onClick={toggleTheme}></i>
+                <i className="bi bi-circle-half" id='toggleTheme' onClick={toggleTheme}></i>
             </nav>
             <nav className="navMobile">
-                <i className="bi bi-chevron-down"></i>
+                <i className={`bi bi-chevron-down ${isMenuOpen ? 'active' : ''}`} id='toggleMenuIcon' onClick={toggleNavTop}></i>
                 <a href='/'>
                     <img src='/img/logo.png' className='logoMob' ></img>
                 </a>
-                <i  className={`bi bi-circle-half ${theme === 'dark' ? 'darkTheme' : ''}`} id='togleThemeMob' ></i>
+                <i  className="bi bi-circle-half"  onClick={toggleTheme} id='toggleThemeMob' ></i>
             </nav>
+            <div className={`containerNavMob ${isMenuOpen ? 'active' : ''}`}>
+                <a className="navLink" href="#">Graduação</a>
+                <a className="navLink" href="#">Segunda Graduação</a>
+                <a className="navLink" href="#">Pós-Graduação</a>
+                <a className="navLink" href="#">Disciplinas isoladas</a>
+                <a className="navLink" href="#">Cursos Grátis</a>
+                <a className="navLink" href="#">Bolsas de estudo</a>
+            </div>
             <header>
                 <a href='/'>
                     <img src='/img/logo.png' className='logo'></img>
@@ -65,17 +94,25 @@ export default function Header() {
             </header>
 
             <div className="headerMob">
-                <div className="hamburguerMenu">
-                    <div className="bar"></div>
-                    <div className="bar"></div>
-                    <div className="bar"></div>
+                <div className="hamburguerMenu" onClick={toggleMenu}>
+                    <div className={`bar ${isHeaderOpen ? 'bar1' : ''}`}></div>
+                    <div className={`bar ${isHeaderOpen ? 'bar2' : ''}`}></div>
+                    <div className={`bar ${isHeaderOpen ? 'bar3' : ''}`}></div>
                 </div>
                 <div className="searchBox Mob">
                     <i className="bi bi-search"></i>
                     <input className='inputBox Mob' type="text" placeholder='Pesquise por um curso'/>
                 </div>
             </div>
+            <div className={`containerHeaderMob ${isHeaderOpen ? 'activeHeader' : ''}`}>
+                <a href='#' className='headerLink'>
+                    Nossas Áreas <i className="bi bi-chevron-down"></i>
+                </a>
+
+                <a href='#' className='headerLink'>Vidas Transformadas</a>
+                <a href='#' className='headerLink'>Blog</a>
+                <a href='#' className='headerLink'>FAQ</a>
+            </div>
         </>
     )
-
 }
